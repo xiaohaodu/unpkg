b3c4fd0cbfbad42fe7be5ccf55eb88a84b4d95c6
杜晓浩 <1923911906@qq.com>
2023-12-10 17:48:23 +0800

    fix: 开发和生产环境 base_url

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,4 +1,3 @@
 // export const baseUrl = 'http://localhost:3000'
 //打包后的文件会直接打包到package/server/public中，在服务器启动时会挂载到服务器上，所以访问路径无需设置
-// export const baseUrl = ''
-export const baseUrl = 'http://localhost:3000'
+export const baseUrl = import.meta.env.PROD ? '' : 'http://localhost:3000'
